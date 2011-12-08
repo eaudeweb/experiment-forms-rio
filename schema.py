44e9a6f508bbcb5e2462918cc6021cd1c87582e2
Alex Morega <alex@grep.ro>
2011-12-08 19:21:33 +0200

    more schema fields

--- schema.py
+++ schema.py
@@ -1,6 +1,16 @@
 import flatland
 
 
+Enum_abc = flatland.Enum.valued('A', 'B', 'C')
+
 class Species(flatland.Form):
     code = flatland.Integer
     name = flatland.String
+    population_resident = flatland.String
+    population_migratory_repro = flatland.String
+    population_migratory_winter = flatland.String
+    population_migratory_transit = flatland.String
+    site_population = Enum_abc
+    site_conservation = Enum_abc
+    site_isolation = Enum_abc
+    site_global = Enum_abc
